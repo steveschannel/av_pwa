c42f240b0a530ec2a40c6fd3f035571928d16e36
steveschannel <kawarii1@gmail.com>
2020-12-06 20:20:08 -0500

    Initial Commit
    Project start - dependencies installed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import 'fontsource-roboto';
+import { Select, MenuItem } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+      >
+        <MenuItem value={10}>GIZ</MenuItem>
+        <MenuItem value={20}>SABA+</MenuItem>
+        <MenuItem value={30}>Artesanal</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+      >
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
     </div>
   );
 }
